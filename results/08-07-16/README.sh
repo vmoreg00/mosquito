a28b48156b78455aa290dafca106a30ab5cde827
Student <student@avalon.uv.es>
2016-07-12 08:42:33 +0200

    bowtie

--- results/08-07-16/README.sh
+++ results/08-07-16/README.sh
@@ -14,23 +14,23 @@ fi
 #para cada muestra ejecutare bowie
 LISTA=(Fe1 Fe2 Fe3 Fe4 Fe5 Fe6 Ma1 Ma2 Ma3 Ma4 Ma5 Ma6 Mol01 Mol02 Mol03 Mol04 Mol05)
 if [ ! -d mappeo ]; then mkdir mappeo; fi
-if [ ! -e culex.1 ]; then
+if [ ! -e culex.1.bt2 ]; then
  #Hago referencia indexada y ordenada
                  bowtie2-build reference.fa culex
 fi
 for i in 0 1; do
-#Para cada una de las muestras	
+#Para cada una de las muestras
 #si no existe el archivo final sam de mapeo
 #ejecuto bowtie
 	if [ ! -e mappeo/${LISTA[$i]}.sam ]; then
 		#Ejecuto
-		bowtie2-align	 --very-sensitive \
+		bowtie2		 --very-sensitive \
 				 -x culex \
 	 	 	 	 -1 /home/student/Documents/mosquito/results/30-06-16/${LISTA[$i]}'_R1.fastq' \
 	 	 	 	 -2 /home/student/Documents/mosquito/results/30-06-16/${LISTA[$i]}'_R2.fastq' \
-		 	 	 -S mappeo/${LISTA[$i]}.sam \ 
+		 	 	 -S mappeo/${LISTA[$i]}.sam \
 				 --rg-id ${LISTA[$i]}
-	fi	
+	fi
 done
 
 
