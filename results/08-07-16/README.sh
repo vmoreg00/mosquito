367a20cbbe87e8c175a11e0fbb1ad6ae2bd43c6b
Student <student@avalon.uv.es>
2016-07-15 12:45:08 +0200

    changes by maria

--- results/08-07-16/README.sh
+++ results/08-07-16/README.sh
@@ -1,59 +1,53 @@
 #!/bin/bash
 #
-#				08-07-16
+#				2016-07-08
 #				----------
-#Check why the mapping wasnt very good
-# we are gonnna use Bowtie2
+#
+# Mapping reads with bowtie2 to Culex quinquefasciatus genome. The reads to map are
+# the fastq files that had been merged and cleaned with cutadapt. They should be in
+# ../2016-07-06b.
+
+FASTQDIR='../2016-07-06b'
 if [ ! -e reference.fa ]; then
 	wget ftp://ftp.ensemblgenomes.org/pub/metazoa/release-31/fasta/culex_quinquefasciatus/dna/Culex_quinquefasciatus.CpipJ2.31.dna.toplevel.fa.gz
 	mv Culex_quinquefasciatus.CpipJ2.31.dna.toplevel.fa.gz reference.fa.gz
 	gunzip reference.fa.gz
 fi
-#Creo una lista con los nombres
-#SI no existe un directorio de mapeo lo creo
-#para cada muestra ejecutare bowie
-LISTA=(Fe1 Fe2 Fe3 Fe4 Fe5 Fe6 Ma1 Ma2 Ma3 Ma4 Ma5 Ma6 Mol01 Mol02 Mol03 Mol04 Mol05)
+
+LISTA=(PipFe1 PipFe2 PipFe3 PipFe4 PipFe5 PipFe6 PipMa1 PipMa2 \
+       PipMa3 PipMa4 PipMa5 PipMa6 Mol01 Mol02 Mol03 Mol04 Mol05)
+
 if [ ! -d mappeo ]; then mkdir mappeo; fi
+
 if [ ! -e culex.1.bt2 ]; then
- #Hago referencia indexada y ordenada
-                 bowtie2-build reference.fa culex
+   bowtie2-build reference.fa culex
 fi
-for i in 0 1 2 3 4 5 6 7 8 9 10; do
-#Para cada una de las muestras
-#si no existe el archivo final sam de mapeo
-#ejecuto bowtie
-	if [ ! -e mappeo/${LISTA[$i]}'r1'.sam ]; then
-		#Ejecuto
-		bowtie2		 --local \
-				 --very-sensitive \
-				 -x culex \
-	 	 	 	 -U /home/student/Documents/mosquito/results/30-06-16/${LISTA[$i]}'_R1.fastq' \
-		 	 	 -S mappeo/${LISTA[$i]}'r1'.sam \
-				 --rg-id ${LISTA[$i]}
+# Now we run the processes on a loop to map the reads with Bowtie2.
+# The first time we made the mapping wasn't very successful so in this time we decided to make a local alignment.
+# Also we  processed the unassembled reads like if they were pair-ends but the mapping was not good again, so
+# finally we decided to process the reads, assembled and unassembled, such as independent ones.
+for i in `seq 0 16`; do
+   if [ ! -e mappeo/${LISTA[$i]}'_r1'.sam ]; then
+      bowtie2 --local \
+              --very-sensitive \
+              -x culex \
+              -U $FASTQDIR/${LISTA[$i]}'_R1_trimmed.fastq',$FASTQDIR/${LISTA[$i]}'_R2_trimmed.fastq',$FASTQDIR/${LISTA[$i]}'_tri$
+              -S mappeo/${LISTA[$i]}'_r1'.sam \
+              --rg-id ${LISTA[$i]}
+   fi
+done
+# Conversion of the SAM files obtained previously to BAM
+
+if [ ! -d BAM ]; then mkdir BAM; fi
+
+for i in `seq 0 16`; do
+	if [ ! -e BAM/${LISTA[$i]}'_r1.bam' ]; then
+		samtools view -bS mappeo/${LISTA[$i]}'_r1'.sam > BAM/${LISTA[$i]}'_r1.bam'
 	fi
-	if [ ! -e mappeo/${LISTA[$i]}'r2'.sam ]; then
-                #Ejecuto
-                bowtie2          --local \
-                                 --very-sensitive \
-                                 -x culex \
-                                 -U /home/student/Documents/mosquito/results/30-06-16/${LISTA[$i]}'_R2.fastq' \
-                                 -S mappeo/${LISTA[$i]}'r2'.sam \
-                                 --rg-id ${LISTA[$i]}
+	if [ ! -e BAM/${LISTA[$i]}'_r2.bam' ]; then
+                samtools view -bS mappeo/${LISTA[$i]}'_r2'.sam > BAM/${LISTA[$i]}'_r2.bam'
+	fi
+        if [ ! -e BAM/${LISTA[$i]}'_merged.bam' ]; then
+                samtools view -bS mappeo/${LISTA[$i]}.sam > BAM/${LISTA[$i]}'_merged.bam'
         fi
 done
-#Una vez obtenidos los archivos en .sam, convertirlos a formato bam (binario de sam)
-#usaremos el programa sam tools
-#como tenemos header el comando usado en samtools es -bS
-#Si no existe directorio BAM lo creamos
-#Para cada elemento de la lista si no existe ya el archivo
-#convertiremos a bam
-if [ ! -d BAM ]; then mkdir BAM; fi
-for i in 0 1 2 3 4 5 6 7 8 9 10; do
-	if [ ! -e BAM/${LISTA[$i]}'r1'.bam ]; then
-		samtools view -bS mappeo/${LISTA[$i]}'r1'.sam > BAM/${LISTA[$i]}'r1'.bam
-	fi
-	if [ ! -e BAM/${LISTA[$i]}'r2'.bam ]; then
-                samtools view -bS mappeo/${LISTA[$i]}'r2'.sam > BAM/${LISTA[$i]}'r2'.bam
-	fi
-done	
-
