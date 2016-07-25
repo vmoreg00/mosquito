0761d3ed2a49646a2c3b7f476456006accf9e4ce
Student <student@avalon.uv.es>
2016-07-25 12:26:53 +0200

    samtools/bcftools

--- results/2016-07-12/README.sh
+++ results/2016-07-12/README.sh
@@ -3,22 +3,22 @@
 #                               12-07-16
 #                               ----------
 #
-#PATHS
+#PATH
 DIR=../2016-07-08
-#Para poder visualizar Snps y otras variaciones
-#necesito convertir el formato BAM a VCF
+# For the visualization of SNPs and INDELS,
+# we need to convert BAM files to VCF.
 LISTA=(PipFe1 PipFe2 PipFe3 PipFe4 PipFe5 PipFe6 PipMa1 PipMa2 PipMa3 PipMa4 PipMa5 PipMa6 Mol01 Mol02 Mol03 Mol04 Mol05)
 
-#Indexar la referencia para SAMtools
-#primero me descargo la referencia
-#despues si no existe ya la referencia indexada, lo indexo con samtools faidx
+# Index the reference for SAMtools
+# First, download the reference and then,
+# if it does not exist, we index it by SAMtools faidx.
 if [ ! -e reference.fa.fai ]; then
         wget ftp://ftp.ensemblgenomes.org/pub/metazoa/release-31/fasta/culex_quinquefasciatus/dna/Culex_quinquefasciatus.CpipJ2.31.dna.toplevel.fa.gz
 	mv Culex_quinquefasciatus.CpipJ2.31.dna.toplevel.fa.gz reference.fa.gz
 	gunzip reference.fa.gz
 	samtools faidx reference.fa
 fi
-#Debo tener indexadas y ordenadas las muestras por posicion y cromosomas
+# Index and sort the reads.
 for i in 0 1 2 3 4 5; do
 	if [ ! -e  ${LISTA[$i]}'_sorted' ]; then
 		samtools sort  $DIR/${LISTA[$i]}.bam  ${LISTA[$i]}'_sorted'
@@ -27,7 +27,8 @@ for i in 0 1 2 3 4 5; do
 		samtools index   ${LISTA[$i]}'_sorted'.bam
 	fi
 done
-#conversion a vcf
+# Finally, we will be able to convert the BAM files obtained at the last step, to VCF.
+# We will obtain a file with the information we called.
 if [ ! -e mosquito.vcf ]; then
 	samtools mpileup -gf reference.fa  *_sorted.bam > mosquito.mpileup
 	bcftools view -Acg mosquito.mpileup > mosquito.vcf
