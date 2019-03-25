a9415745ab9ab6521abc47286ee1dc135cd190c0
Victor <vicmogon@alumni.uv.es>
2019-03-25 16:29:38 +0100

    Sliding Windows ABBA BABA test has been carried out.

--- results/2019-02-23/src/ABBA_BABA_SlidingWindows.R
+++ results/2019-02-23/src/ABBA_BABA_SlidingWindows.R
@@ -31,96 +31,87 @@ chrom_table$cumm.length <- cumsum(chrom_table[, 2])
 # Sliding window analysis (concurrent)
 cl <- makeCluster(cores)
 clusterExport(cl = cl, varlist = c("window_size", "overlap", "min_data", "step",
-                                   "chrom_table", "freqs", "f.stat",
+                                   "chrom_table", "chrom_lengths", "freqs", "f.stat",
                                    "abba.baba.test", "D.stat", "abba", "baba"))
 D.sliding <- t(parallel::parSapply(
   cl = cl,
-  X = seq(from = 1, by = step,
-          length.out = round(max(chrom_table$cumm.length) / step, 0)),
+  X = as.character(chrom_table$V1),
   function(x){
-    # Determine the start and end position of the window IN THE GENOME
-    ## Start and end position of the window in the genome
-    begin <- x
-    end <- x + window_size
-    ## First chromosome in window
-    chr_begin <- which(chrom_table$cumm.length > begin)
-    chr_begin <- as.character(chrom_table[, 1][chr_begin[1]])
-    ## Start position in the genome (3rd column of chrom_table)
-    if(begin == 1){
-      pos_begin <- 1
+    # If the contig is shorter than 5 * window_size, it is discarded
+    if(any(chrom_lengths[x] < 5 * window_size, # Short contig
+           sum(freqs$chrom == x) < 100)){      # Few or no SNPs in contig
+      return(rep(NULL, 10))
     } else {
-      pos_begin <- which(chrom_table[, 1] == chr_begin)
-      pos_begin <- abs(chrom_table[pos_begin - 1, 3] - begin)
+      windows_begin <- seq(1, chrom_lengths[x], by = window_size)
+      D <- data.frame()
+      k <- 1
+      while(k < length(windows_begin)){
+        # Start position of the window in freqs
+        i <- windows_begin[k]
+        freq_begin <- which(freqs$chrom == x & freqs$pos >= i)[1]
+        # Last position of the window in freqs
+        j <- i + window_size
+        freq_end <- tail(which(freqs$chrom == x & freqs$pos <= j), 1)
+        # Number of SNPs
+        n.snp <- freq_end - freq_begin
+        if(n.snp < 100){
+          # Extention of the window and re-computation of the windows' begin
+          freq_end <- freq_begin + 100
+          offset <- freqs$pos[freq_end] - j
+          windows_begin <- windows_begin + offset
+        }
+        if(j > chrom_lengths[x]){
+          D <- rbind(D, rep(NA, 10))
+        } else {
+          # Execute ABBA_BABA test in 2 cases
+          D_case1 <- c(x, i, abba.baba.test(freqs[freq_begin:freq_end,],
+                                            "mixS2.S3", "molM1", "pipA4",
+                                            sliding = TRUE))
+          D_case2 <- abba.baba.test(freqs[freq_begin:freq_end,],
+                                    "mixS2.S3", "molM1", "molA1",
+                                    sliding = TRUE)
+          D <- rbind(D, c(D_case1, D_case2), stringsAsFactors = F)
+          colnames(D) <- c("chr", "pos",
+                           "D_1", "f_1", "ABBA_1", "BABA_1",
+                           "D_2", "f_2", "ABBA_2", "BABA_2")
+        }
+        k <- k + 1
+      }
+      return(D)
     }
-    ### If chr_begin has no SNP (not in freqs), the next will be selected
-    while(!(chr_begin %in% freqs$chrom)){
-      chr_begin <- 
-        as.character(chrom_table[which(chrom_table[, 1] == chr_begin) + 1, 1])
-      pos_begin <- 1
-    }
-    ## Last chromosome in window
-    chr_end <- which(chrom_table$cumm.length > end)
-    chr_end <- as.character(chrom_table[, 1][chr_end[1]])
-    ## End position in the genome (3rd column of chrom_table) 
-    pos_end <- which(chrom_table[, 1] == chr_end)
-    pos_end <- abs(chrom_table[pos_end - 1, 3] - end)
-    ### If chr_end has no SNP (not in freqs), the previous will be selected
-    while(!(chr_end %in% freqs$chrom)){
-      chr_end <-
-        as.character(chrom_table[which(chrom_table[, 1] == chr_end) - 1, 1])
-      pos_end <- chrom_table[which(chrom_table[, 1] == chr_end), 2]
-    }
-
-    # Determine which SNPs are between start and end position in freqs_table
-    ## First SNP in window
-    freq_begin <- which(freqs$chrom == chr_begin & freqs$pos >= pos_begin)[1]
-    ### If freq_begin does not exist, the first of the next chr is selected
-    if(is.na(freq_begin)){
-      freq_begin <- tail(which(freqs$chrom == chr_begin), 1) + 1
-    }
-    ## Last SNP in window
-    freq_end <- which(freqs$chrom == chr_end & freqs$pos <= pos_end)[1]
-    ### If freq_end does not exist, the last SNP of the previous chr is selected
-    if(is.na(freq_end)){
-      freq_end <- which(freqs$chrom == chr_end)[1] - 1
-    }
-
-    # ABBA BABA test in 2 cases
-    if(freq_end - freq_begin > min_data){
-      D_case1 <- c(x, abba.baba.test(freqs[freq_begin:freq_end,],
-                                     "mixS2.S3", "molM1", "pipA4",
-                                     sliding = TRUE))
-      D_case2 <- abba.baba.test(freqs[freq_begin:freq_end,],
-                                "mixS2.S3", "molM1", "molA1",
-                                sliding = TRUE)
-    } else {
-      D_case1 <- c(x, 0, 0, 0, 0)
-      D_case2 <- c(0, 0, 0, 0) # pos is ommited as it is the same as in case 1
-    }
-    D <- c(D_case1, D_case2)
-    D
   }))
 stopCluster(cl)
 
 # Format the table and save it
-D.sliding <- as.data.frame(D.sliding)
-names(D.sliding) <- c("pos", "D_1", "f_1", "ABBA_1", "BABA_1", 
-                      "D_2", "f_2", "ABBA_2", "BABA_2")
-write.table(D.sliding, file = "abba_baba_slidingWindows.tsv", sep = "\t", 
+D.sliding1 <- data.frame()
+for(i in D.sliding){
+  D.sliding1 <- rbind(D.sliding1, i)
+}
+for(i in 2:ncol(D.sliding1)){
+  D.sliding1[, i] <- as.numeric(D.sliding1[, i])
+}
+## Differences between the f statistic in both cases
+D.sliding1$diff_f <- D.sliding1$f_1 - D.sliding1$f_2
+## "Genomic position" if contigs were sorted. For representation purposes.
+## The offset of the extended windows is very low, so it can be asumed that
+## all the windows are 100000bp length.
+D.sliding1$pos_genomic <- seq(from = 1, by = 100000,
+                              length.out = nrow(D.sliding1))
+## Save
+write.table(D.sliding1, file = "abba_baba_slidingWindows.tsv", sep = "\t",
             quote = F)
 
 # Plot the results
 facets <- 5
-D.sliding$facet <- cut(D.sliding$pos, facets, seq(1, facets))
+D.sliding1$facet <- cut(D.sliding1$pos, facets, seq(1, facets))
 
-g <- ggplot(data = D.sliding, aes(x = pos)) +
-  geom_line(aes(y = D_1, colour = "p3 = pipiens from Aleksin")) +
-  geom_line(aes(y = D_2, colour = "p3 = molestus from Aleksin")) +
+g <- ggplot(data = D.sliding1, aes(x = pos_genomic)) +
+  geom_line(aes(y = diff_f)) +
   facet_wrap(vars(facet), ncol = 1, scales = "free_x",
              strip.position = "right") +
   scale_x_continuous(labels = function(x) format(x, scientific = TRUE),
                      name = "Genome position") +
-  scale_y_continuous(name = "D statistic", limits = c(-1, 1)) +
+  scale_y_continuous(name = "$f_1 - f_2$", limits = c(-1, 1)) +
   theme_bw() +
   theme(legend.position = "top", legend.title = element_blank())
 ggsave(filename = "abba_baba_slidingWindows.png", plot = g, device = "png",
